938980cfd3bd9df934b17bac65209b3d83b3933f
Zhen Data <zhenyang.9261@gmail.com>
2019-10-05 20:24:00 -0400

    Added trivia questions and basic coding structure in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,77 @@
+
+var questions = {
+    q1: {
+        question: "The Alamo, site of the famous battle, is a tourist attraction in what Texas city?",
+        answers: ["Austin", "Houston", "San Antonio", "Brownsville"],
+        correct: 2
+    },
+    q2: {
+        question: "Which of the following is not a favorable adjective when discussing wine?",
+        answers: ["fat", "flinty", "leggy", "vigorous"],
+        correct: 0
+    },
+    q2: {
+        question: "What soft-drink company introduced the brand Slice?",
+        answers: ["Dr. Pepper", "Coca Cola", "Seven Up", "Pepsico"],
+        correct: 1
+    },
+    q2: {
+        question: "Catherine the Great ruled what country?",
+        answers: ["England", "France", "Germany", "Russia"],
+        correct: 3
+    },
+    q2: {
+        question: "What was the next state after the original 13 to be admitted to the United States?",
+        answers: ["Florida", "Kentucky", "Vermont", "West Virginia"],
+        correct: 2
+    },
+    q2: {
+        question: "Where was the UN headquarters located prior to them moving to Manhattan's East Side?",
+        answers: ["San Francisco", "Long Island, New York", "Geneva, Switzerland", "Paris, France"],
+        correct: 1
+    },
+    q2: {
+        question: "Which of the following countries does not border Israel?",
+        answers: ["Egypt", "Jordan", "Saudi Arabia", "Syria"],
+        correct: 2
+    },
+    q2: {
+        question: "Which one of the following countries is not known as one of the Baltic states?",
+        answers: ["Albania", "Estonia", "Latvia", "Lithuania"],
+        correct: 0
+    },
+    q2: {
+        question: "Which two planets are most similar in size diameter wise?",
+        answers: ["Mars and Mercury", "Venus and Earth", "Uranus and Neptune", "Jupiter and Saturn"],
+        correct: 1
+    },
+    q2: {
+        question: "In which century was the greatest number of chemical elements discovered?",
+        answers: ["17th", "18th", "19th", "20th"],
+        correct: 2
+    }
+}
+
+
+var trivia = {
+
+    // Attributes
+
+
+    /*
+     * Function: when "Start Game" is clicked
+     */
+
+
+
+    /*
+     * Function: when an answer is clicked
+     */
+}
+
+$(document).ready(function() {
+
+    // Button Listeners
+
+})
+
